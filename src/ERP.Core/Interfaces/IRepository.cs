4058d890d3a04dd5d9e11ad37d91845f26e7107c
Sid Prabaharan <137012018+sidprabaharan@users.noreply.github.com>
2025-03-19 10:11:29 -0400

    Create IRepository.cs

---
+++ src/ERP.Core/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ERP.Core.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<IReadOnlyList<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null);
+    }
+}
