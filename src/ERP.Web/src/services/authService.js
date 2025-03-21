872dd9d6d02fedfc7eec74794c3ad4bd3e66f2bb
Sid Prabaharan <137012018+sidprabaharan@users.noreply.github.com>
2025-03-21 10:57:20 -0400

    Create authService.js

---
+++ src/ERP.Web/src/services/authService.js
@@ -0,0 +1,11 @@
+import api from './apiService';
+
+export const loginUser = async (username, password) => {
+  try {
+    const response = await api.post('/auth/login', { username, password });
+    return response.data;
+  } catch (error) {
+    console.error('Login error:', error);
+    throw error;
+  }
+};
